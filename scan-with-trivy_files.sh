9f782b012862da5cc95e20b6d3e8f150afd632e2
kongyew <kongyew@gmail.com>
2022-06-21 20:25:35 -0700

    readme

---
+++ scan-with-trivy_files.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+for d in testimage/*; do
+	# Skip non-directories.
+	if [[ ! -d "$d" ]]; then continue; fi
+
+	# Skip if the directory doesn't have a Dockerfile.
+	if [[ ! -f "$d/Dockerfile" ]]; then continue; fi
+
+	# Skip these non-working examples.
+	if [[ $d == "examples/*" ]]; then continue; fi
+
+	# Scan the image, or fail.
+
+	trivy fs $d 
+
+done
