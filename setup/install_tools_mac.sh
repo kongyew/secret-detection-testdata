c1c75e6a00a14eb1e22984c46d67f8b4b36da3b2
kongyew <kongyew@gmail.com>
2022-06-21 19:50:51 -0700

    initial

---
+++ setup/install_tools_mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+go install github.com/deepfence/SecretScanner@latest
+
+brew install git-secrets
+
+
+
+# git clone https://github.com/trufflesecurity/trufflehog.git
+# cd trufflehog; go install
+#brew tap trufflesecurity/trufflehog
+#brew install trufflehog
+
